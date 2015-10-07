a1ef558d8270e1987de35fbd0c5dbfc82eb3e36f
gagarine <perdrisat@gmail.com>
2015-10-07 07:23:42 +0200

    code cleanup

--- app/sites/all/themes/prozent/template.php
+++ app/sites/all/themes/prozent/template.php
@@ -4,16 +4,18 @@
  * template.php
  */
 
- /**
-  * Preprocess variables for node.tpl.php
-  *
-  * @see node.tpl.php
-  */
- function learn_preprocess_node(&$variables) {
+/**
+* Preprocess variables for node.tpl.php
+*
+* @see node.tpl.php
+*/
+function learn_preprocess_node(&$variables) {
 
-   // That will let you use a template file like: node--[type|nodeid]--teaser.tpl.php
-   if($variables['view_mode'] == 'teaser') {
-     $variables['theme_hook_suggestions'][] = 'node__' . $variables['node']->type . '__teaser';
-   }
+// That will let you use a template file like: node--[type|nodeid]--teaser.tpl.php
+if($variables['view_mode'] == 'teaser') {
+ $variables['theme_hook_suggestions'][] = 'node__' . $variables['node']->type . '__teaser';
+}
 
- }
+}
+
+
