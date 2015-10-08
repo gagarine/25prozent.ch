ff7623a22086a526367ab970bb67f0d0b30ef51b
gagarine <perdrisat@gmail.com>
2015-10-08 15:15:33 +0200

    validation of phone number in front page

--- app/sites/all/themes/prozent/templates/system/page.tpl.php
+++ app/sites/all/themes/prozent/templates/system/page.tpl.php
@@ -152,11 +152,18 @@
 
         <h1 style="margin-top: 120px;" class="text-center">2015 Swiss federal election</h1>
         <h2 class="text-center"> – Foreigners get to vote – </h2>
-        <?php if($logged_in): ?>
-          <p class="text-center"><a type="button" href="/electoral-list" class="btn btn-primary btn-lg">Let me vote</a></p>
-        <?php else: ?>
+
+        <?php if($logged_in && user_access('create vote content')){ ?>
+          <p class="text-center"><a type="button" href="/electoral-list" class="btn btn-primary btn-lg">Vote Now!</a></p>
+        <?php
+        } elseif($logged_in && !user_access('create vote content')){
+          global $user;
+          ?>
+            <p class="text-center">To vote, first please validate your mobile number so we know your are in switzerland.</p>
+          <p class="text-center"><a type="button" href="/user/<?php echo $user->uid; ?>/edit/mobile" class="btn btn-warning btn-lg">Ok. Let me validate my mobile number.</a></p>
+        <?php }else{ ?>
           <p class="text-center"><a type="button" href="/user/register" class="btn btn-primary btn-lg">Let me vote</a></p>
-        <?php endif; ?>
+        <?php } ?>
         <p class="text-center">25% of people living in Switzerland are  foreigner. We will report their voice.</p>
 
       <?php else: ?>
